19e230430315b2422992c2a0c37729b26e2a2753
Stein Hammer <sslegga@gmail.com>
2017-10-15 16:54:38 +0200

    added Yesterday

--- bin/midi-analyzer.pl
+++ bin/midi-analyzer.pl
@@ -1,8 +1,11 @@
 #!/usr/bin/perl
+use FindBin;
+use lib "$FindBin::Bin/../../utillities-perl/lib";
 use lib '/media/data/perlbrew/perls/perl-5.26.0/lib/site_perl/5.26.0/';
 use Mojo::Base -strict;
 use MIDI; # uses MIDI::Opus et al
 use Data::Dumper;
+use SH::PrettyPrint;
 
 =head1 DESCRIPTION
 
@@ -14,12 +17,16 @@ velocity = a value 0 to 127
 
 =cut
 foreach my $one (@ARGV) {
-   my $opus = MIDI::Opus->new({ 'from_file' => $one, 'no_parse' => 1 });
-   my @tracks = $opus->tracks;
-   print "$one has ", scalar( @tracks ). " tracks\n";
-   my $data = $tracks[0]->data;
-   my @events = MIDI::Event::decode( \$data, {exclude=>['control_change','set_tempo','sysex_f0'] } );
-   print Dumper @tracks;
-   print Dumper @events;
+    my $opus = MIDI::Opus->new({ 'from_file' => $one, 'no_parse' => 1 });
+    my @tracks = $opus->tracks;
+    print "$one has ", scalar( @tracks ). " tracks\n";
+    my $data = $tracks[0]->data;
+    my @events = MIDI::Event::decode( \$data, {exclude=>['control_change','set_tempo','sysex_f0'] } );
+#   print Dumper @tracks;
+    say ref $events[0] ;
+    SH::PrettyPrint::print_arrayofarrays $_ for @events;
+#    print Dumper @events;
+# print     $opus->dump( { flat=>1} );
+   print Dumper  \%MIDI::number2note;
 }
 exit;
