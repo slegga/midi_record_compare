9e7bf7f546648731bf2db63de920485278a6643e
Slegga <steihamm@gmail.com>
2018-07-08 10:34:41 +0200

    Fixed one error when turn off piano

--- bin/record-midi-music.pl
+++ bin/record-midi-music.pl
@@ -127,7 +127,7 @@ sub alsa_read {
     	($on_time - ($self->action->last_event_starttime||$self->action->tune_starttime))};
     my $place = 'start';
     $place = 'slutt' if $alsaevent[0] == 6;
-    $place = 'slutt' if $alsaevent[7][2] == 0;
+    $place = 'slutt' if $alsaevent[0] == 7 && $alsaevent[7][2] == 0;
 
     printf("%-6s %s %3d %.3f\n",$place,Model::Utils::Scale::value2notename($self->action->tune->scale,$alsaevent[7][1]),$alsaevent[7][2],$alsaevent[8]{dtime_sec}) if $alsaevent[0] == 6 || $alsaevent[0] == 7;
     my $event = Model::Utils::alsaevent2midievent(@alsaevent);
