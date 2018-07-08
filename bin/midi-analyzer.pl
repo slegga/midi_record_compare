d4440a1150f026e37a6f3e9bac3a83a10fc415e4
Stein Hammer <steihamm@gmail.com>
2018-07-08 17:17:11 +0200

    Fix output

--- bin/midi-analyzer.pl
+++ bin/midi-analyzer.pl
@@ -23,7 +23,7 @@ foreach my $one (@ARGV) {
     my @events = MIDI::Event::decode( \$data, {exclude=>['control_change', 'sysex_f0'] } );
 #   print Dumper @tracks;
     say ref $events[0] ;
-    SH::PrettyPrint::print_arrayofarrays $_ for @events;
+    SH::PrettyPrint::print_arrays $_ for @events;
 #    print Dumper @events;
  print     $opus->dump( { flat=>1} );
 #   print Dumper  \%MIDI::number2note;
