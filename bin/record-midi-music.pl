9a0cb804f7517ae0aadc6ccb5ad213c6cf964b00
Slegga <steihamm@gmail.com>
2018-08-23 20:28:22 +0200

    rewrite commands need testing

--- bin/record-midi-music.pl
+++ bin/record-midi-music.pl
@@ -51,7 +51,16 @@ has loop  => sub { Mojo::IOLoop->singleton };
 has stdin_loop => sub { Mojo::IOLoop::Stream->new(\*STDIN)->timeout(0) };
 has silence_timer=> -1;
 has input_object => sub { Model::Input::ALSA->new };
-
+has commands => sub{[
+    [[qw/h help/],     0, 'This help text', sub{$_[0]->do_help}],
+    [[qw/l list/],     1, 'List saved tunes.', sub{$_[0]->action->do_endtune;			$_[0]->action->do_list($_[1])}],
+    [[qw/p play/],     1, ' Play last tune. If none defaults to the not ended play.', sub{$_[0]->action->do_endtune; $_[0]->action->do_play($_[1])}],
+    [[qw/s save/],     1, 'Save play to disk as notes.', sub{$_[0]->action->do_endtune;	$_[0]->action->do_save($_[1])}],
+    [[qw/c comp/],     1, 'Compare last tune with given name. If not name then test with --comp argument', sub{   $_[0]->action->do_comp($_[1])}],
+    [[qw/sm savemidi/],1, 'Save as midi file. Add .midi if not present in name.', sub{$_[0]->action->do_endtune;  $_[0]->action->do_save_midi($_[1])}],
+    [[qw/q quit/],     1, 'End session.',sub{$_[0]->do_quit}],
+    [[qw/defaults/],    1, 'Stop last tune and start on new.', sub{$_[0]->action->do_endtune()}],
+]};
 has action => sub {Model::Action->new};
 my @COPY_ARGV = @ARGV;
 my ( $opts, $usage, $argv ) =
@@ -65,10 +74,11 @@ __PACKAGE__->new->main(@ARGV) if !caller;
 
 sub main {
     my $self = shift;
-    if (! defined $self->input_object->port()
-        && (! exists $ENV{MOJO_MODE} || $ENV{MOJO_MODE} eq 'dry-run' ) && ! grep {'--dry-run' eq $_} @_) {
+    if (! defined $self->input_object->port()) {
+        if ((! exists $ENV{MOJO_MODE} || $ENV{MOJO_MODE} eq 'dry-run' ) && ! grep {'--dry-run' eq $_} @_) {
             printf '%s ## %s', ($ENV{MOJO_MODE}//'__UNDEF__'),join(',',@_);
             die "Did not find the midi input stream! Need port number.";
+        }
     } else {
         $self->input_object->init();
     }
@@ -114,7 +124,7 @@ sub register_midi_event {
     # $place = 'slutt' if $alsaevent[0] == 7 ;
     # $place = 'slutt' if $alsaevent[0] == 6 && $alsaevent[7][2] == 0;
 
-    printf("%-7s %-3s %3d %.3f\n",$event->[0]
+    printf("%-8s %-3s %3d %.3f\n",$event->[0]
     ,(defined($event->[3]) ? Model::Utils::Scale::value2notename($self->action->tune->scale,$event->[3]):'__UNDEF__')
     ,($event->[4]//0),
     $event->[2]//0);
@@ -144,49 +154,51 @@ sub stdin_read {
         ($cmd, $name)=split /\s+/, $bytes;
     }
     $self->silence_timer(-1);
-    if (defined $cmd && grep { $cmd eq $_ } ('h','help')) {
-        $self->print_help();
-    } else {
-        if(!defined $cmd) {
-            if ($opts->comp) {
-                $self->action->do_comp($opts->comp);
-            } else {
-                $self->action->do_endtune();
-            }
-        }elsif (grep {$cmd eq $_} 'q','quit' ) {
-        	$self->do_quit;
-        } elsif (grep {$cmd eq $_} ('c','comp')) {
-            $self->action->do_comp($name);
-        } else {
-            $self->action->do_endtune;
-            if (grep { $cmd eq $_ } ('s','save')) {
-                $self->action->do_save($name);
-            } elsif (grep { $cmd eq $_} ('sm','savemidi')) {
-                $self->action->do_save_midi($name);
-            } elsif (grep { $cmd eq $_} ('p','play')) {
-                $self->action->do_play($name);
-            } elsif (grep {$cmd eq $_} ('l','list')) {
-                $self->action->do_list($name);
-            }
-        }
+#    if (defined $cmd && grep { $cmd eq $_ } ('h','help')) {
+#        $self->print_help();
+#    } else {
+	if(!defined $cmd) {
+		if ($opts->comp) {
+		$self->action->do_comp($opts->comp);
+		} else {
+		$self->action->do_endtune();
+		}
+	} else {
+		for my $c(@{$self->commands}) {
+		if (grep {$cmd eq $_} @{$c->[0]} ) {
+		$c->[3]->($self, $name);
+		last;
+		}
+		}
+	}
+
+#        }
+#        } elsif (grep {$cmd eq $_} 'q','quit' ) {
+#        	$self->do_quit;
+#        } elsif (grep {$cmd eq $_} ('c','comp')) {
+#            $self->action->do_comp($name);
+#        } else {
+#            $self->action->do_endtune;
+#            if (grep { $cmd eq $_ } ('s','save')) {
+#                $self->action->do_save($name);
+#            } elsif (grep { $cmd eq $_} ('sm','savemidi')) {
+#                $self->action->do_save_midi($name);
+#            } elsif (grep { $cmd eq $_} ('p','play')) {
+#                $self->action->do_play($name);
+#            } elsif (grep {$cmd eq $_} ('l','list')) {
+#                $self->action->do_list($name);
+#            }
+    #    }
         $self->action->midi_events([]); # clear history
         $self->input_object->reset_time();
-    }
+   # }
 }
 
 sub print_help {
-    print q'
-    h,help          This help text
-    s,save [NAME]   Save play to disk as notes.
-    p,play [NAME]   Play last tune. If none defaults to the not ended play.
-    l,list [REGEXP] List saved tunes.
-    c,comp [NAME]   Compare last tune with given name. If not name then test with --comp argument
-    sm [NAME]       Save as midi file. Add .midi if not present in name.
-    q,quit			End session.
-
-    defaults        Stop last tune and start on new.
-
-';
+	my $self = shift;
+	for my $cmd(@{$self->commands}) {
+		printf "         %-16s %-10s  %-20s\n",join(',',@{$cmd->[0]}), ' [STR]'x$cmd->[1], $cmd->[2];
+	}
 }
 
 sub do_quit {
@@ -194,5 +206,4 @@ sub do_quit {
 	say "Goodbye";
 	$self->loop->stop_gracefully;
 }
-
 1;
