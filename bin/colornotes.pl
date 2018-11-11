c51d3cf550af1155d47c3797dba2af24d79aadd0
Stein Hammer <steihamm@gmail.com>
2018-11-11 16:18:18 +0100

    fix

--- bin/colornotes.pl
+++ bin/colornotes.pl
@@ -65,10 +65,11 @@ sub main {
     	# code for split left and right
     	# split
     	# look back to se if ok
-    	if ($line eq '') {
-    		...;
-    	} else {
-    		print color('green');
+    	my $note = $line->to_hash_ref;
+    	if ($note->{note} > 56) {# right
+    		print color('blue');
+    	} else { # left
+    		print color('red');
     	}
     	print $line;
     }
@@ -76,57 +77,3 @@ sub main {
 }
 
 __PACKAGE__->new(options_cfg=>{extra=>1})->main();
-
-__END__
-#!/usr/bin/env perl
-
-use MIDI; # uses MIDI::Opus et al
-use Data::Dumper;
-use FindBin;
-use lib "$FindBin::Bin/../../utilities-perl/lib";
-use lib "$FindBin::Bin/../lib";
-use SH::ScriptX;
-use Mojo::Base 'SH::ScriptX';
-#use Carp::Always;
-use Model::Tune;
-
-
- - cleanup a note txt file
-
-
-Ment to be used to convert samples to blueprints.
-Remove comments and add new ones.
-Can change scale.
-
-
-option 'extend=s', 'Extend these periods to next valid length. Takes , separated list';
-option 'scale=s', 'Set scale. Convert from old to given scale. Example c_dur';
-option 'ticsprbeat=i', 'Number of tics. Examle 6.';
-#,{return_uncatched_arguments => 1});
- sub main {
-    my $self = shift;
-    my @e = $self->extra_options;
-    say Dumper \@e;
-    my $filename = ($self->extra_options)[0];
-    die "No file given" if ! $filename;
-    my $tune = Model::Tune->from_note_file($filename);
-    my $new_scale;
-    if ($self->scale) {
-        $new_scale = $self->scale;
-    } else {
-        $new_scale = Model::Utils::Scale::guess_scale_from_notes($tune->notes);
-    }
-    if ($new_scale ne $tune->scale) {
-        $tune->scale($new_scale);
-    }
-
-    if ($self->ticsprbeat) {
-        $tune->denominator($self->ticprbeat);
-    }
-
-    say "$tune";
-    $tune->to_note_file; # will write notes based on $tune->scale
-}
-
-__PACKAGE__->new(options_cfg=>{extra=>1})->main();
-
