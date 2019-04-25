b25bbce09369dd63070e46e4d5217782ac35561a
Stein Hammer <steihamm@gmail.com>
2019-04-25 18:03:37 +0200

    xml virker nesten. Mangler bare join tied noter

--- script/xml-test.pl
+++ script/xml-test.pl
@@ -27,41 +27,49 @@ if(0) {
 	my $xml_converter = XML::Hash->new();
 	my $xml =  $xml_converter->fromXMLStringtoHash('lg-69390178.xml');
 
-#	say join("\n",keys %{$xml->{'score-partwise'}});
-#p $xml->{'score-partwise'}->{part}->{measure};
-for my $n(@{ $xml->{'score-partwise'}->{part}->{measure} }) {
-#	p $n;
-#	next;
-	if (0) {
-		for my $x (@{$n->{note}}) {
-			if (! exists $x->{'default-x'}) {
-				warn "unhandeled";
-				p $x;
+	#	say join("\n",keys %{$xml->{'score-partwise'}});
+	#p $xml->{'score-partwise'}->{part}->{measure};
+
+	# Loop thorugh hash, create array og hashes of notes
+	my $prev = {x=>0,d=>10000};
+	my @notes;
+	for my $n(@{ $xml->{'score-partwise'}->{part}->{measure} }) {
+	#	p $n;
+	#	next;
+		if (0) {
+			for my $x (@{$n->{note}}) {
+				if (! exists $x->{'default-x'}) {
+					warn "unhandeled";
+					p $x;
+				}
 			}
 		}
-	}
-	my $prev = {x=>0,d=>10000};
-	for my $m(sort {int($a->{'default-x'}/12) <=> int($b->{'default-x'}/12)} grep {exists $_->{'default-x'}} @{$n->{note}}) {
-#		p $m;
-		my $x = int($m->{'default-x'}/12);
-		my $d = $m->{duration}->{text};
-		#say join(' ',$x, $m->{'default-y'}, $m->{duration}->{text},j($m->{pitch}));
-		# 0;1;G4        # 0.0-1/2
-		my $pause=0;
-		if (!$prev->{x}) {
+		for my $m(sort {int($a->{'default-x'}/12) <=> int($b->{'default-x'}/12)} grep {exists $_->{'default-x'}} @{$n->{note}}) {
+	#		p $m;
+			my $x = int($m->{'default-x'}/12);
+			my $d = $m->{duration}->{text};
+			#say join(' ',$x, $m->{'default-y'}, $m->{duration}->{text},j($m->{pitch}));
+			# 0;1;G4        # 0.0-1/2
+			my $pause=0;
+			if (!$prev->{x}) {
+				$prev->{x} = $x;
+			}
+			if ( $prev->{x} == $x ) {
+				if ($prev->{d} > $d) {
+					$prev->{d} = $d;
+				}
+				$pause = 0;
+			} else {
+				$pause = $prev->{d};
+			}
+
+			printf "%s;%s;%s%s #%s %s\n", $pause, $m->{duration}->{text}, $m->{pitch}->{step}->{text}, $m->{pitch}->{octave}->{text}, $m->{'default-x'}, $m->{'default-y'};
+			$prev->{d} = $m->{duration}->{text};
 			$prev->{x} = $x;
 		}
-		if ( $prev->{x} == $x ) {
-			if ($prev->{d} > $d) {
-				$prev->{d} = $d;
-			}
-			$pause = 0;
-		} else {
-			$pause = $prev->{d};
-		}
-
-		printf "%s;%s;%s%s\n", $pause, $m->{duration}->{text}, $m->{pitch}->{step}->{text}, $m->{pitch}->{octave}->{text};
 	}
-}
-#	$xml_converter->fromXMLStringtoHash($xml);
+
+	# print output based on array of hashes
+	# for my $note(@notes) {
+	# }
 }
